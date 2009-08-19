e6bf04586798c022eb554c47ee29bca0ddd98320
liza31337 <liza31337@e08d0fb5-4147-0410-a205-bba44f1f51a3>
2009-08-19 15:18:27 +0000

    Fix for [#151]

--- trunk/bookworm/library/views.py
+++ trunk/bookworm/library/views.py
@@ -518,7 +518,7 @@ se from epubcheck, ignoring: %s' % d)
         if valid_resp is None:
             # We got nothing useful from the validator (oops)
             pass
-        elif valid_resp:
+        elif len(valid_resp) == 0:
             message.append(_(u"<p>(epubcheck thinks this file is valid, so this may be a Bookworm error)</p>"))
         else:
             message.append(_(ubr/>'.join(epub_error_list)
